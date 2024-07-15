7ec190618e6b7ba7cdfde04e7e509dc692b4e917
Xelagurd <alexander.drugakov@mail.ru>
2024-07-15 13:15:18 +0300

    Added home page
    ### What's done:
    - Added home page with main image.

---
+++ src/test/kotlin/xelagurd/pizzaproject/HomeControllerTest.kt
@@ -0,0 +1,29 @@
+package xelagurd.pizzaproject
+
+import org.hamcrest.Matchers
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+
+@WebMvcTest(HomeController::class)
+class HomeControllerTest {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    @Throws(Exception::class)
+    fun testHomePage() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.view().name("home"))
+            .andExpect(
+                MockMvcResultMatchers.content().string(
+                    Matchers.containsString("Welcome to...")
+                )
+            )
+    }
+}
