59cc5ac35a7aeb58776f2e46e07b89e25a7ef03b
Xelagurd <alexander.drugakov@mail.ru>
2024-07-15 13:09:00 +0300

    Initial commit

---
+++ src/main/kotlin/xelagurd/pizzahub/PizzaHubApplication.kt
@@ -0,0 +1,11 @@
+package xelagurd.pizzahub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PizzaHubApplication
+
+fun main(args: Array<String>) {
+    runApplication<PizzaHubApplication>(*args)
+}
