70c0ae55e86e02f7f4ec2744677bd1fe30ddd265
Xelagurd <56447239+Xelagurd@users.noreply.github.com>
2024-07-15 13:16:32 +0300

    Added home page (#1)
    ### What's done:
    - Added home page with main image.

---
+++ src/main/kotlin/xelagurd/pizzaproject/HomeController.kt
@@ -0,0 +1,12 @@
+package xelagurd.pizzaproject
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HomeController {
+    @GetMapping("/")
+    fun home(): String {
+        return "home"
+    }
+}
