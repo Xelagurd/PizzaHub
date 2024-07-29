5e9398711b596e17ee7394f9393544e60652c3b6
Xelagurd <alexander.drugakov@mail.ru>
2024-07-29 18:59:14 +0300

    Added advanced configuration properties
    ### What's done:
    - Added advanced configuration properties

---
+++ src/main/kotlin/xelagurd/pizzahub/controller/utils/OrderProps.kt
@@ -0,0 +1,16 @@
+package xelagurd.pizzahub.controller.utils
+
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+import org.springframework.validation.annotation.Validated
+
+@Component
+@ConfigurationProperties(prefix = "pizza.orders")
+@Validated
+class OrderProps(
+    @field:Min(value = 5, message = "Must be between 5 and 25")
+    @field:Max(value = 25, message = "Must be between 5 and 25")
+    var pageSize: Int = 20
+)
